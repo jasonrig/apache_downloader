d7bd2b4909208f43ca861d7f46b47678f72b3677
Jason Rigby <jason.rigby@paypay-corp.co.jp>
2024-01-15 15:25:36 +0900

    remove reference to the mirror selection URL

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='apache-downloader',
-    version='0.1.2',
+    version='0.1.3',
     packages=['apache_downloader'],
     scripts=['bin/apache-dl'],
     url='',
